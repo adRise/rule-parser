73643162ec83117b4c44097d9c843f6b13c4093c
wanghaofei90 <wanghaofei90@gmail.com>
2016-03-08 22:51:37 -0800

    finish test case

--- test.js
+++ test.js
@@ -26,7 +26,7 @@ test('and, or, not works', t => {
     'not (num > 10) and (date > 01/01/2017) or (num > 10)',
     { num: 10, date: new Date(), num: 20 }
   );
-  
+
   t.true(v1);
   t.true(v2);
   t.true(v3);
@@ -60,7 +60,7 @@ test('validator works', t => {
     },
     variable: function (inputVar, data) {
       if (!this[inputVar]) {
-        throw new Error(inputVar + ' in invalid');
+        throw new Error(inputVar + ' is invalid');
       }
       this.preVar = inputVar;
       return data[inputVar];
@@ -77,23 +77,75 @@ test('validator works', t => {
         return self.string(item);
       });
     },
+    date: function (input) {
+      try{
+        return new Date(input);
+      } catch (e){
+        throw new Error(input + ' is not a valid date');
+      }
+    },
     preVar: null,
-    date: true,
     country: new Set(['AF', 'AX', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR', 'AM', 'AW', 'AU', 'AT', 'AZ', 'BS', 'BH', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BQ', 'BA', 'BW', 'BV', 'BR', 'IO', 'BN', 'BG', 'BF', 'BI', 'KH', 'CM', 'CA', 'CV', 'KY', 'CF', 'TD', 'CL', 'CN', 'CX', 'CC', 'CO', 'KM', 'CG', 'CD', 'CK', 'CR', 'CI', 'HR', 'CU', 'CW', 'CY', 'CZ', 'DK', 'DJ', 'DM', 'DO', 'EC', 'EG', 'SV', 'GQ', 'ER', 'EE', 'ET', 'FK', 'FO', 'FJ', 'FI', 'FR', 'GF', 'PF', 'TF', 'GA', 'GM', 'GE', 'DE', 'GH', 'GI', 'GR', 'GL', 'GD', 'GP', 'GU', 'GT', 'GG', 'GN', 'GW', 'GY', 'HT', 'HM', 'VA', 'HN', 'HK', 'HU', 'IS', 'IN', 'ID', 'IR', 'IQ', 'IE', 'IM', 'IL', 'IT', 'JM', 'JP', 'JE', 'JO', 'KZ', 'KE', 'KI', 'KP', 'KR', 'KW', 'KG', 'LA', 'LV', 'LB', 'LS', 'LR', 'LY', 'LI', 'LT', 'LU', 'MO', 'MK', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT', 'MH', 'MQ', 'MR', 'MU', 'YT', 'MX', 'FM', 'MD', 'MC', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM', 'NA', 'NR', 'NP', 'NL', 'NC', 'NZ', 'NI', 'NE', 'NG', 'NU', 'NF', 'MP', 'NO', 'OM', 'PK', 'PW', 'PS', 'PA', 'PG', 'PY', 'PE', 'PH', 'PN', 'PL', 'PT', 'PR', 'QA', 'RE', 'RO', 'RU', 'RW', 'BL', 'SH', 'KN', 'LC', 'MF', 'PM', 'VC', 'WS', 'SM', 'ST', 'SA', 'SN', 'RS', 'SC', 'SL', 'SG', 'SX', 'SK', 'SI', 'SB', 'SO', 'ZA', 'GS', 'SS', 'ES', 'LK', 'SD', 'SR', 'SJ', 'SZ', 'SE', 'CH', 'SY', 'TW', 'TJ', 'TZ', 'TH', 'TL', 'TG', 'TK', 'TO', 'TT', 'TN', 'TR', 'TM', 'TC', 'TV', 'UG', 'UA', 'AE', 'GB', 'US', 'UM', 'UY', 'UZ', 'VU', 'VE', 'VN', 'VG', 'VI', 'WF', 'EH', 'YE', 'ZM', 'ZW']),
     platform: new Set(['ios', 'iphone', 'ipad', 'amazon', 'xboxone', 'xbox360', 'web', 'roku', 'samsung'])
   };
 
-  const v1 = parser.parse('country in ["US","CA"] and date > 04/01/2016', { country: 'CA', date : new Date('04/02/2016')}, validator);
-
-  var testFn = function() {
+  const testFn1 = function () {
     try {
       parser.parse('country in ["AAAA","CA"] and date > 04/01/2016', {
         country: 'CA',
         date: new Date('04/02/2016')
       }, validator);
-    } catch (e){
+    } catch (e) {
       throw new Error(e);
     }
   };
-  t.throws(testFn);
+  t.throws(testFn1, 'Error: AAAA is not a valid country');
+
+  const testFn2 = function () {
+    try {
+      parser.parse('counttry in ["US","CA"] and date > 04/01/2016 or platform = "opera"', {
+        country: 'CA',
+        date: new Date('04/02/2016')
+      }, validator);
+    } catch (e) {
+      throw new Error(e);
+    }
+  };
+  t.throws(testFn2, 'Error: counttry is invalid');
+
+  const testFn3 = function () {
+    try {
+      parser.parse('country in ["US","CA"] and date > 04/01/2016 or platform = "opera"', {
+        country: 'CA',
+        date: new Date('04/02/2016')
+      }, validator);
+    } catch (e) {
+      throw new Error(e);
+    }
+  };
+  t.throws(testFn3, 'Error: opera is not a valid platform');
+
+  const testFn4 = function () {
+    try {
+      parser.parse('country in ["US","CA"] and date > 04/01/2016 or platfrom = "amazon"', {
+        country: 'CA',
+        date: new Date('04/02/2016')
+      }, validator);
+    } catch (e) {
+      throw new Error(e);
+    }
+  };
+  t.throws(testFn4, 'Error: platfrom is invalid');
+
+  const testFn5 = function () {
+    try {
+      parser.parse('country in ["US","CA"] and date > 23/23/2016', {
+        country: 'CA',
+        date: new Date('04/02/2016')
+      }, validator);
+    } catch (e) {
+      throw new Error(e);
+    }
+  };
+  t.throws(testFn5);
 });

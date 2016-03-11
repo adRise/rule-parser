27a579a8c9493c7ea94bf1e07e729208ce6b5e7d
wanghaofei90 <wanghaofei90@gmail.com>
2016-03-10 16:57:21 -0800

    separate term and expression

--- play.js
+++ play.js
@@ -42,9 +42,10 @@ var jison = require("jison");
 var bnf = fs.readFileSync("rule.jison", "utf8");
 var parser = new jison.Parser(bnf);
 
-var output  = parser.parse('country in [] and date > 12/23/2015', {
-  country: 'CA',
-  date: new Date('04/02/2016')
+var output  = parser.parse('country in "US" and date > 12/23/2015 and platform = "amazon"', {
+  country: 'US',
+  date: new Date('04/02/2016'),
+  platform: 'amazon',
 }, validator);
 
 console.log(output);
